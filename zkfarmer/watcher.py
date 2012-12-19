a81ef8db2f2e9c3071be4529a19bd2a5047f87dd
Vincent Bernat <vincent.bernat@dailymotion.com>
2012-12-19 19:06:14 +0100

    WIP.
    This does not work because zc.zk does handle session expiration on
    most operations. This is also incomplete, since only the exporter has
    been converted.

--- zkfarmer/watcher.py
+++ zkfarmer/watcher.py
@@ -15,6 +15,8 @@ from watchdog.observers import Observer
 
 from .utils import serialize, unserialize
 
+import logging
+logger = logging.getLogger(__name__)
 
 class ZkFarmWatcher(object):
     def __init__(self):
@@ -37,39 +39,68 @@ class ZkFarmExporter(ZkFarmWatcher):
     def __init__(self, zkconn, root_node_path, conf, updated_handler=None, filter_handler=None):
         super(ZkFarmExporter, self).__init__()
         self.watched_paths = {}
+        self.zkconn = zkconn
 
+        node_names = self.zkconn.children(root_node_path)
+        node_names(self.node_watcher)
         while True:
             with self.cv:
-                try:
-                    node_names = zkconn.get_children(root_node_path, self.get_watcher(root_node_path))
-                except NoNodeException:
-                    zkconn.create_recursive(root_node_path, '', zc.zk.OPEN_ACL_UNSAFE)
-                    continue
-                except ConnectionLossException:
-                    self.wait()
-                    continue
                 new_conf = {}
+                logger.debug("looking for changes in %s" % root_node_path)
                 for name in node_names:
                     subnode_path = '%s/%s' % (root_node_path, name)
-                    info = unserialize(zkconn.get(subnode_path, self.get_watcher(subnode_path))[0])
+                    info = dict(self.zkconn.properties(subnode_path))
                     if not filter_handler or filter_handler(info):
                         new_conf[name] = info
+                logger.debug("writing back changes")
                 conf.write(new_conf)
                 if updated_handler:
                     updated_handler()
                 self.wait()
 
-    def watcher(self, handle, type, state, path):
+    def node_watcher(self, children=None):
+        if children is None:
+            # Will be handled by the parent
+            return
         with self.cv:
-            if path in self.watched_paths:
-                del self.watched_paths[path]
+            logger.debug("adding or deleting node in %r" % children)
+            paths = []
+            for c in children:
+                paths.append("%s/%s" % (children.path, c))
+            # Remove children that don't exist anymore
+            rc = 0
+            for path in self.watched_paths.keys():
+                if not path.startswith("%s/" % children.path):
+                    continue
+                if path not in paths:
+                    logger.debug("removing path %s from watched paths" % path)
+                    del self.watched_paths[path]
+                    rc += 1
+            if rc != 0:
+                logger.debug("removed %d nodes" % rc)
+
+            # Add new children
+            rc = 0
+            for path in paths:
+                if path not in self.watched_paths:
+                    logger.debug("adding new path %s to watched paths" % path)
+                    children = self.zkconn.children(path)
+                    children(self.node_watcher)
+                    properties = self.zkconn.properties(path)
+                    properties(self.property_watcher)
+                    self.watched_paths[path] = (children, properties)
+            if rc != 0:
+                logger.debug("added %d nodes" % rc)
+            # Notify everyone of updates
             self.notify()
 
-    def get_watcher(self, path):
-        if path not in self.watched_paths:
-            self.watched_paths[path] = True
-        return self.watcher
-
+    def property_watcher(self, property=None):
+        if property is None:
+            # Will be handled by the parent
+            return
+        with self.cv:
+            logger.debug("modification of node %r" % property)
+            self.notify()
 
 class ZkFarmJoiner(ZkFarmWatcher):
     def __init__(self, zkconn, root_node_path, conf):
